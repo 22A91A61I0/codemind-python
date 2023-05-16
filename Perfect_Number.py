220969da18278cc2cf295e62d78807ea519da18c
divyasri <119840453+22A91A61I0@users.noreply.github.com>
2023-05-16 20:30:28 +0530

    def pos(n):     sum=0     for i in range(1,n):         if n%i==0:             sum+=i     if sum==n:         print("True")     else:         print("False") n=int(input()) pos(n)

---
+++ Perfect_Number.py
@@ -0,0 +1,11 @@
+def pos(n):
+    sum=0
+    for i in range(1,n):
+        if n%i==0:
+            sum+=i
+    if sum==n:
+        print("True")
+    else:
+        print("False")
+n=int(input())
+pos(n)
